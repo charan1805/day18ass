92b2230dec89740bae6176590b97749347a39ec7
charan1805 <sreecharanmudhiraj@gmail.com>
2023-09-12 22:28:57 +0530

    first class

---
+++ WebAPIExample/WebAPIExample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAPIExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebAPIExample/WebAPIExample/Models/Task.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace WebAPIExample.Models
+
+    [Table("Task")]
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueTime { get; set; }
+    }
+}
